028a22530c9db841694ac6987e21d8903d6bd6bb
Sam <sampat.palani@gmail.com>
2017-09-28 18:21:44 +0000

    Added motd recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,12 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'Apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
 service 'Apache2' do
-	service_name 'httpd'
+	service_name package
 	action [:start,:enable]
 end
+
--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,4 +1,4 @@
 file 'default www' do 
-	path '/etc/www/html/index.html'
+	path '/var/www/html/index.html'
 	content 'Hello World'
 end
