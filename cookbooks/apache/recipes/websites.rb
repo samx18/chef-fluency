304129ad7d7af27bb91c6b28490c60c5acd2eefd
Sam <sampat.palani@gmail.com>
2017-10-02 18:31:56 +0000

    Updated recipes to use search

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -2,3 +2,14 @@ file 'default www' do
 	path '/var/www/html/index.html'
 	content 'Hello World'
 end
+
+#Search within recipe based on a role
+
+webnodes = search('node','role:web')
+
+# Iterate over each node and put
+
+webnodes.each do |node|
+	puts node
+end 
+
